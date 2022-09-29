74ef49d6c344334d200e0227a0a42cb0f14dbd80
tomassssl <tomassliep@gmail.com>
2022-09-29 15:16:30 +0300

    Car class, some methods

--- public/index.php
+++ public/index.php
@@ -2,4 +2,17 @@
 
 declare(strict_types=1);
 
-//require __DIR__ . '/../bootstrap/app.php';
+require_once '../src/Car.php';
+
+// HW: Driver (name, surname, age)
+
+
+$car = (new Car(odometer: 1000,number_plate: 'LN-333',fuelConsumption: 14.5))
+    ->addKilometers(200)
+    ->addKilometers(500);
+
+$liters = $car->getConsumedLiters();
+
+$car = null;
+
+var_dump($liters);
